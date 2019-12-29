c1430fb406bf0daf3c5e299a7dbfac9c6caa0ba0
Gregory Diaz <gregdiaz8624@yahoo.com>
2019-12-29 19:01:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,13 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  [0,1,2,3]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  add_element_to_end_of_array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
